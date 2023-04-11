fa648f04b3712c1abe23d079679adeb03cda1c82
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-04-11 21:33:28 +0700

    Add files via upload

---
+++ Array tipe data char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char name[20];
+	
+	cout << "Plese enter your name: ";
+	cin >> name;
+	
+	cout << "Hello, " << name << "!" << endl;
+
+}
